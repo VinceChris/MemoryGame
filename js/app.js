30c3631b560790d7849b285145b4c50859eedd60
Vincent Christifano <vince.christifano@gmail.com>
2018-12-17 08:44:46 -0600

    Moved match noMatch into funcion, added winconditn

--- js/app.js
+++ js/app.js
@@ -30,9 +30,9 @@ function shuffle(array) {
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *d  - if the list already has another card, check to see if the two cards match
+ *d   + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *d   + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
@@ -41,6 +41,7 @@ function shuffle(array) {
 let allCards = document.querySelectorAll('.card');
 // Create an array for all open cards
 let openCards = [];
+let matchCounter = 0;
 //Add event flip funcitonality to cards using click event listener.
 allCards.forEach(function(card){
     card.addEventListener('click',function(event){
@@ -48,34 +49,43 @@ allCards.forEach(function(card){
         if (openCards.length <= 2) {// flip cards using show and open class.
             card.classList.add('show', 'open');
             if (openCards.length == 2){
-                switch(openCards[0].firstElementChild.className == openCards[1].firstElementChild.className){
-                    case true:
-                        // If true add 'match' class, remove 'open' 'show' class 
-                        console.log("Match Found!");
-                        openCards[0].classList.add('match');
-                        openCards[0].classList.remove('open', 'show');
-                        openCards[1].classList.add('match');
-                        openCards[1].classList.remove('open', 'show');
-                        // remove cards from openCards array
-                        openCards = [];
-                        break;
-                    case false:
-                        // if false add 'noMatch'class
-                        openCards[0].classList.add('noMatch');
-                        openCards[1].classList.add('noMatch');
-                        // Wait a period of time before removing open, show, noMatch classes
-                        setTimeout(function(){ 
-                            openCards[0].classList.remove('open', 'show', 'noMatch');
-                            openCards[1].classList.remove('open', 'show', 'noMatch');
-                            // remove cards from openCards array
-                            openCards = [];
-                        }, 1500);
-                        
-                        break
-                }
+                if (openCards[0].firstElementChild.className == openCards[1].firstElementChild.className){
+                    matchActions();
+                } else { noMatchActions();}   
             }
-        
-        }           
+        }
+        finalScore();
+    });           
 
-    });
 });
+
+
+function matchActions(){
+    // If true add 'match' class, remove 'open' 'show' class 
+    openCards[0].classList.add('match');
+    openCards[0].classList.remove('open', 'show');
+    openCards[1].classList.add('match');
+    openCards[1].classList.remove('open', 'show');
+    // remove cards from openCards array
+    openCards = [];
+    matchCounter++; 
+}
+
+function noMatchActions(){
+    // if false add 'noMatch'class
+    openCards[0].classList.add('noMatch');
+    openCards[1].classList.add('noMatch');
+    // Wait a period of time before removing open, show, noMatch classes
+    setTimeout(function(){ 
+        openCards[0].classList.remove('open', 'show', 'noMatch');
+        openCards[1].classList.remove('open', 'show', 'noMatch');
+        // remove cards from openCards array
+        openCards = [];
+    }, 1500);
+}
+
+function finalScore(){
+    if (matchCounter == 8){
+        console.log('Final Score is 100%!')
+    }
+}
