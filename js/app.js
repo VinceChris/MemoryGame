7fb73f1bc9b62713da3e47ee37e896e06d408df6
Vincent Christifano <vince.christifano@gmail.com>
2018-12-18 22:31:54 -0600

    Removes allCards classes, shuffles places cards.

--- js/app.js
+++ js/app.js
@@ -1,51 +1,46 @@
 /*
  * Create a list that holds all of your cards
  */
-let allCards = document.querySelectorAll('.card');
-let cardList = [];
+let allCards = document.querySelectorAll('.card'); // Array holds the cards
+let cardList = []; // Array to hold the card type.
+
 allCards.forEach(function(cardX,index){
-    cardList[index] = cardX.firstElementChild;
+    cardList[index] = cardX.firstElementChild.className;
+    console.log('cardList initialized ' + cardList[index]);
 });
-
-function displayChild(array1){
-    array1.forEach(function(item,index){
-       console.log(array1[index].firstElementChild); 
-    });
-}
-
-function displayCard(array2){
-    array2.forEach(function(item,index){
-       console.log(array2[index]); 
-    });
-}
-
-function updateChildren(cardArray, classArray){
-    cardArray.forEach(function(card, index){
-        //console.log(element.firstElementChild.innerText + " set equal to" + array4.innerText);
-        card.firstElementChild.className = classArray[index].className;
-    });
-}
-//Perform the shuffle
-updateChildren(allCards, cardList);
 
 /*
  * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
+ *d  - shuffle the list of cards using the provided "shuffle" method below
+ *d  - loop through each card and create its HTML
+ *d  - add each card's HTML to the page
  */
-cardList = shuffle(cardList);
 
-cardList.forEach(function(cardY,item,array){
-    allCards[item].firstElementChild = cardY;
-});
+//Initialize the game on first click. Shuffle cards and place on board.
+let clickCounter = 0;
+function isFirstClick(){
+    ++clickCounter
+    if (clickCounter == 1){
+        cardList = shuffle(cardList);
+        removeCardsClass(allCards);
+        placeShuffledCards();
+    } 
+}
 
-
+function removeCardsClass(){
+    allCards.forEach(function(card){        
+        card.firstElementChild.className = '';
+    });
+}
+function placeShuffledCards(){
+    cardList.forEach(function(card,index){  
+        allCards[index].firstElementChild.className = card; 
+    });
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -53,7 +48,6 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
@@ -77,12 +71,13 @@ let moveCounter = document.getElementsByClassName('moves');
 //Add card flip funcitonality using click event listener.
 allCards.forEach(function(card){
     card.addEventListener('click',function(event){
+        isFirstClick(); // shuffle cards at start of game
+        // ***add checks for clicking matched card.***
         openCards.push(card);// Add card to openCard array.
         if (openCards.length <= 2) {// flip cards using show and open class.
             card.classList.add('show', 'open');
             if (openCards.length == 2){
-                //increment the move counter
-                ++moveCounter[0].innerText 
+                ++moveCounter[0].innerText //increment the move counter 
                 if (openCards[0].firstElementChild.className == openCards[1].firstElementChild.className){
                     matchActions();
                 } else {
@@ -104,23 +99,21 @@ function matchActions(){
         openCards[0].classList.remove('open', 'show');
         openCards[1].classList.add('match');
         openCards[1].classList.remove('open', 'show');
-        // remove cards from openCards array
-        openCards = [];
+        openCards = [];     // remove cards from openCards array
     }, 500);
     matchCounter++; 
 }
 
 function noMatchActions(){
-    // if false add 'noMatch'class
+    // flash cards red to indicate no match.
     setTimeout(function(){ 
         openCards[0].classList.add('noMatch');
         openCards[1].classList.add('noMatch');
-        // Wait a period of time before removing open, show, noMatch classes
+        // Wait a period of time before hiding card again.
         setTimeout(function(){ 
             openCards[0].classList.remove('open', 'show', 'noMatch');
             openCards[1].classList.remove('open', 'show', 'noMatch');
-            // remove cards from openCards array
-            openCards = [];
+            openCards = [];     // remove cards from openCards array
         }, 1500);
     }, 250);
 }
@@ -129,9 +122,13 @@ function finalScore(){
     if (matchCounter == allCards.length/2){
         setTimeout(function(){ 
             window.alert('Final Score is 100%!');
+            matchCounter = 0;                   // reset the card match counter 
+            moveCounter[0].innerText = 0;       // reset the move counter
             allCards.forEach(function(card){
-                card.classList.remove('match');
+                card.classList.remove('match'); // hide all the cards
             })
+            clickCounter = 0;                   //reset the clickCounter
         }, 510);
     }
 }
+
